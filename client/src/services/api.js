b4e2f6987bf2c73c3b66b87381d0f47c8606b5fa
Ubuntu <ubuntu@ip-172-31-45-208.ap-south-1.compute.internal>
2026-09-11 17:41:54 +0000

    Add Docker and Kubernetes deployment configuration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: '/api'
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('insight_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
